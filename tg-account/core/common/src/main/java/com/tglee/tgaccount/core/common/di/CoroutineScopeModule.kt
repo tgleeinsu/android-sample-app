67c9a6496a395dbea66af8d36ec8fb47dd08d1f3
IngJobs <tglee951008@gmail.com>
2026-06-27 17:11:08 +0900

    feat(core:common): ApplicationScope Hilt 모듈 추가
    - `@ApplicationScope` Qualifier 정의
    - `CoroutineScope(SupervisorJob + Dispatchers.IO)` 제공 (SingletonComponent)
    - Repository에서 StateFlow를 업데이트하는 등 앱 수명 주기에 맞춘 비동기 작업 지원

---
+++ tg-account/core/common/src/main/java/com/tglee/tgaccount/core/common/di/CoroutineScopeModule.kt
@@ -0,0 +1,27 @@
+package com.tglee.tgaccount.core.common.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+/** 앱 전체 수명의 CoroutineScope. repository 가 StateFlow 를 채울 때 사용한다. */
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApplicationScope
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineScopeModule {
+
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun provideApplicationScope(): CoroutineScope =
+        CoroutineScope(SupervisorJob() + Dispatchers.IO)
+}
